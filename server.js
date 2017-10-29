38d004b74e3ddf5a3f445853d1e7ef245eaee337
KurYur <k.k.ten.10tacle.0107@leaf.ocn.ne.jp>
2017-10-29 18:44:50 +0900

    一通り変更

--- server.js
+++ server.js
@@ -1,11 +1,78 @@
 'use strict';
-
+// SERVER SETTING
 const http = require('http');
-const PORT = 3000;
+const https = require('https');
+const crypto = require('crypto');
+const Twitter = require('twitter');
 
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
-    res.end('HELLO NODE!');
-}).listen(PORT);
+// TWITTER SETTING
+const Twitter = require('twitter');
+const tw = new Twitter({
+  consumer_key:        process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key:    process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
-console.log(`Server running at ${PORT}`);
+// LINE SETTING
+const HOST            = 'api.line.me';
+const CH_SECRET       = process.env.LINE_CHANNEL_SECRET;
+const CH_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN;
+const USER_ID         = process.env.LINE_USER_ID;
+const PUSH_PATH       = '/v2/bot/message/multicast';
+const SIGNATURE       = crypto.createHmac('sha256', CH_SECRET);
+const PORT            = 3000;
+const pushClient = (userId, SendMessageObject) => {
+    let postDataStr = JSON.stringify({ to: userId, messages: SendMessageObject });
+    let options = {
+        host: HOST,
+        port: 443,
+        path: PUSH_PATH,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'X-Line-Signature': SIGNATURE,
+            'Authorization': `Bearer ${CH_ACCESS_TOKEN}`,
+            'Content-Length': Buffer.byteLength(postDataStr)
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        let req = https.request(options, (res) => {
+                    let body = '';
+                    res.setEncoding('utf8');
+                    res.on('data', (chunk) => {
+                        body += chunk;
+                    });
+                    res.on('end', () => {
+                        resolve(body);
+                    });
+        });
+
+        req.on('error', (e) => {
+            reject(e);
+        });
+        req.write(postDataStr);
+        req.end();
+    });
+};
+
+// Get Official Account's Tweet
+const TARGET = ['1549889018','2968069742','864400939125415936']
+var stream = tw.stream('statuses/filter', { track :'1549889018',follow :'2968069742',follow :'864400939125415936'});
+stream.on('data', function (data,err){
+  if(TARGET.indexOf(data.user.id_str) >= 0) {
+        let PushSendMessageObject = [{
+          type: 'text',
+          text: data.user.name + '\n' + data.text
+        }];
+
+        pushClient([USER_ID], PushSendMessageObject)
+          .then((body) => {
+            console.log(body);
+          }, (e) => {console.log(e)});
+
+        console.log(data.user.name + '\n' + data.text);
+
+  }
+});
